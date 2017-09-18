157fdf4fd8733e796e6c29c0546605f7ba6af58a
Janarthanan <janaravi962@gmail.com>
2017-09-18 20:17:42 +0530

    solved bugs
    found one bug  which is when we press enter it in textarea it save it and it will not delete that item solved now.

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
-var myform = document.getElementById("myform");
-//myform.addEventListener("submit", saveInfo);
-window.addEventListener("load", showTodoItems);
-var saveBtn = document.getElementById("submit");
+var myform = document.getElementById("myform"),
+saveBtn = document.getElementById("submit"),
+cancelBtn = document.getElementById("cancel");
 saveBtn.addEventListener("click", saveInfo);
-var cancelBtn = document.getElementById("cancel");
+window.addEventListener("load", showTodoItems);
 cancelBtn.addEventListener("click", clearForm);
 function clearForm(e) {
     myform.reset();
-}
+};
 
 function saveInfo(e) {
     e.preventDefault();
     var dateValue = document.getElementById("inputdeadline").value;
     var todoValue = document.getElementById("textarea").value;
-    
+    todoValue = todoValue.replace(/\n/g,"   ");
     if ( dateValue > 24 || dateValue <= 0) {
         myform.reset();
         return false;
-    }
+    };
     var todoObj = {
         todoValue,
         dateValue
-    }
+    };
     if (!(todoValue)) {
         myform.reset();
         return false;
-    }
+    };
     
     if (localStorage.getItem("localTodoItem") === null) {
         var todoArray = [];
@@ -41,21 +40,21 @@ function saveInfo(e) {
             return a.dateValue - b.dateValue;
         });
         localStorage.setItem("localTodoItem", JSON.stringify(todoArray));
-    }
+    };
     showTodoItems();
     myform.reset();
-}
+};
 
 function deleteTodoItem(content) {
     var todoArray = JSON.parse(localStorage.getItem("localTodoItem"));
     for (var i = 0; i < todoArray.length; i++) {
         if (todoArray[i].todoValue == content) {
             todoArray.splice(i, 1);
-        }
+        };
         localStorage.setItem("localTodoItem", JSON.stringify(todoArray));
         showTodoItems();
-    }
-}
+    };
+};
 function showTodoItems() {
     var todoArray = JSON.parse(localStorage.getItem("localTodoItem"));
     var result = document.getElementById("showcontent");
@@ -71,6 +70,6 @@ function showTodoItems() {
             }
            dl =  dl % 12 || 12;
             result.innerHTML += "<div class='well'>" +"<p>"+ content +"</p>"+ "<a class='dl'>" + '<span>Last hour to complete :</span>' + dl + ampm + "</a>" + "<a class='btn btn-danger del' onclick ='deleteTodoItem( \"" + content + "\")'>" + "x" + "</a>" + "</div>";
-        }
-    }
-}
+        };
+    };
+};
